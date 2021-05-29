fa987b0a607a09a834ca4617e14a12dcea12f8e0
inPraiseOfBoredom <anastazy.andreeva@gmail.com>
2021-05-29 15:41:00 +0000

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-content>
+      <div class="home">
+        <v-tabs v-model="tab" fixed-tabs background-color="pink">
+          <v-tab v-for="item in items" :key="item">
+            {{ item }}
+          </v-tab>
+        </v-tabs>
+
+        <Calendar />
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import Calendar from "@/components/Calendar.vue";
+export default {
+  name: "Home",
+  components: {
+    Calendar,
+  },
+  data() {
+    return {
+      tab: null,
+      items: ["Meal plan", "ingredients", "shopping list"],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
